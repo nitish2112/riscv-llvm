cc8ee5a61a47c08522c9adb81604a76c75f67aaf
Shiva <shiva@andestech.com>
2017-08-01 15:24:09 +0800

    Update ELF.h include path

--- lib/Target/RISCV/MCTargetDesc/RISCVTargetStreamer.cpp
+++ lib/Target/RISCV/MCTargetDesc/RISCVTargetStreamer.cpp
@@ -14,12 +14,12 @@
 #include "RISCVMCExpr.h"
 #include "RISCVMCTargetDesc.h"
 #include "RISCVTargetStreamer.h"
+#include "llvm/BinaryFormat/ELF.h"
 #include "llvm/MC/MCContext.h"
 #include "llvm/MC/MCSectionELF.h"
 #include "llvm/MC/MCSubtargetInfo.h"
 #include "llvm/MC/MCSymbolELF.h"
 #include "llvm/Support/CommandLine.h"
-#include "llvm/Support/ELF.h"
 #include "llvm/Support/ErrorHandling.h"
 #include "llvm/Support/FormattedStream.h"
 
